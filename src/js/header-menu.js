d6aa0eee97cc7f1e9819fc4ce53ef932c56337cd
Mykola <ipadikys@gmail.com>
2025-04-09 00:59:06 +0200

    feat: finished my mobile part. (Fix images and hovers)

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -1,1 +1,17 @@
-console.log('header-menu.js loaded');
+const burgerBtn = document.querySelector(".burgerBtn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn-js");
+
+function modalToggle() {
+    modal.classList.toggle("show");
+    document.body.classList.toggle("no-scroll");
+}
+
+burgerBtn.addEventListener("click", modalToggle);
+closeBtn.addEventListener("click", modalToggle);
+
+/* window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modalToggle();
+    }
+}); */
