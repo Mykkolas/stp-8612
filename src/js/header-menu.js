75fa9edec2cf25862c092dc5a9da57dc45cde248
Mykola <ipadikys@gmail.com>
2025-04-09 20:03:42 +0200

    changed modal

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -1,17 +1,24 @@
 const burgerBtn = document.querySelector(".burgerBtn");
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("close-btn-js");
-
+const links = modal.querySelectorAll("a");
 function modalToggle() {
     modal.classList.toggle("show");
     document.body.classList.toggle("no-scroll");
 }
 
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        const targetId = link.getAttribute("href");
+
+        modalToggle(); 
+
+        setTimeout(() => {
+            window.location.hash = targetId;
+        }, 300); 
+    });
+});
 burgerBtn.addEventListener("click", modalToggle);
 closeBtn.addEventListener("click", modalToggle);
 
-/* window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        modalToggle();
-    }
-}); */
